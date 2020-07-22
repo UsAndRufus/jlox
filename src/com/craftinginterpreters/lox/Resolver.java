33e67df68b2bab50d5cf59a295f03d51845ffd77
Edward <edward,bennigsen@protonmail.com>
2020-07-22 22:03:25 +0100

    up to 11.3.2

---
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -0,0 +1,65 @@
+package com.craftinginterpreters.lox;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+    private Interpreter interpreter;
+    private final Stack<Map<String, Boolean>> scopes = new Stack<>();
+
+    Resolver(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        beginScope();
+        resolve(stmt.statements);
+        endScope();
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        declare(stmt.name);
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer);
+        }
+        define(stmt.name);
+        return null;
+    }
+
+    private void beginScope() {
+        scopes.push(new HashMap<>());
+    }
+
+    private void endScope() {
+        scopes.pop();
+    }
+
+    private void declare(Token name) {
+        if (scopes.isEmpty()) return;
+        scopes.peek().put(name.lexeme, false);
+    }
+
+    private void define(Token name) {
+        if (scopes.isEmpty()) return;
+        scopes.peek().put(name.lexeme, true);
+    }
+
+    private void resolve(Stmt statement) {
+        statement.accept(this);
+    }
+
+    private void resolve(Expr expression) {
+        expression.accept(this);
+    }
+
+    private void resolve(List<Stmt> statements) {
+        for (Stmt stmt : statements) {
+            resolve(stmt);
+        }
+    }
+}
