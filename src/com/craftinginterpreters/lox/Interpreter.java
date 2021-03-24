ebf555307cb58c9c65a6182e4ea35f0ef6d576c3
Edward B <edward.bennigsen@protonmail.com>
2021-03-24 23:45:32 +0000

    Set properties on object and fix gets

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -111,11 +111,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitGetExpr(Expr.Get expr) {
         Object object = evaluate(expr.object);
-        if (object instanceof LoxClass) {
+        if (object instanceof LoxInstance) {
             return ((LoxInstance) object).get(expr.name);
         }
 
-        throw new RuntimeError(expr.name, "Only instances have properties.");
+        throw new RuntimeError(expr.name, "Only instances have properties");
     }
 
     @Override
@@ -139,6 +139,20 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return evaluate(expr.right);
+    }
+
+    @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name,
+                    "Only instances have fields.");
+        }
+
+        Object value = evaluate(expr.value);
+        ((LoxInstance)object).set(expr.name, value);
+        return value;
     }
 
     @Override
